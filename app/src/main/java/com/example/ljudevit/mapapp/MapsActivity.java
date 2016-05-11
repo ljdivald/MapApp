6c1e8a5825256000ead8400d967d5be3ed97e77d
Ljudevit <ljudevit.divald@fer.hr>
2016-05-11 19:29:06 +0200

    maknuti nepotrebni komentari

--- app/src/main/java/com/example/ljudevit/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/ljudevit/mapapp/MapsActivity.java
@@ -31,16 +31,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -70,28 +60,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         Toast.LENGTH_LONG).show();
             }
         };
-        // Add a marker in Sydney and move the camera
+
         LatLng zagreb = new LatLng(48, 16);
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mMap.setOnMapClickListener(mapClickListener);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(zagreb));
     }
 
-
-  /*  @Override
-   public void onMapClick(LatLng latLng) throws IOException {
-        Geocoder gcd = new Geocoder(this, Locale.getDefault());
-        List<android.location.Address> addresses = gcd.getFromLocation(latLng.latitude, latLng.longitude, 1);
-
-        String countryName = "";
-
-        if (addresses.size() > 0)
-        {
-            countryName=addresses.get(0).getCountryName();
-        }
-
-        Toast.makeText(MapsActivity.this,
-                countryName,
-                Toast.LENGTH_LONG).show();
-    }*/
 }
